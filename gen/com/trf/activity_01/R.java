d41bc7a8c8f12953f3f6bf940e760adac6ab60e5
XXFatai <trf669045207@126.com>
2013-02-28 19:48:39 +0800

    初步了解了activity

--- gen/com/trf/activity_01/R.java
+++ gen/com/trf/activity_01/R.java
@@ -25,9 +25,13 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int dummy_button=0x7f070002;
+        public static final int dummy_button=0x7f070004;
         public static final int fullscreen_content=0x7f070000;
-        public static final int fullscreen_content_controls=0x7f070001;
+        public static final int fullscreen_content2=0x7f070001;
+        public static final int fullscreen_content3=0x7f070002;
+        public static final int fullscreen_content_controls=0x7f070003;
+        public static final int my_button=0x7f070005;
+        public static final int my_button1=0x7f070006;
     }
     public static final class layout {
         public static final int activity_activity01=0x7f030000;
@@ -36,6 +40,8 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int app_name=0x7f050000;
         public static final int dummy_button=0x7f050001;
         public static final int dummy_content=0x7f050002;
+        public static final int my_button=0x7f050003;
+        public static final int my_text=0x7f050004;
     }
     public static final class style {
         /** 
